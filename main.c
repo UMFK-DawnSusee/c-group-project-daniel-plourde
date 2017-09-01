442a1d4f3ebe351d0759b0961f5cbd8c98654ef6
Dawn Susee <dawnsusee@gmail.com>
2017-09-01 07:10:42 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Dawn
+ *
+ * Created on August 31, 2017, 9:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Let's play Hangman!");
+    return (EXIT_SUCCESS);
+}
+
